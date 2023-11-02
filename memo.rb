51388d31cacb3a65d2d6c97743df410bf2ecdbd5
komiya7249 <119317460+komiya7249@users.noreply.github.com>
2023-11-02 20:43:31 +0900

    Update memo.rb

--- memo.rb
+++ memo.rb
@@ -10,26 +10,26 @@
   
  elsif memo_type == 1
   puts "メモを新規作成します。拡張子を除いたファイル名を入力してください"
-  @file_name = gets.to_s
+  file_name = gets.to_s
   puts"メモしたい内容を記入してください"
   puts"完了したらCtrl+Dを押します"
   
-  CSV.open("#{@file_name}.csv",'a') do |input|
-   @user_inputs = readlines
-  input << @user_inputs
- end
+  CSV.open("#{file_name}.csv",'a') do |input|
+   user_inputs = readlines
+   input << user_inputs
+  end
   
   puts "作成が完了しました"   
    
  elsif memo_type == 2
   puts"既存のメモに書き込みを行います。拡張子を除いたファイル名を入力してください"
-  @file_name = gets.to_s
+  file_name = gets.to_s
   puts"メモしたい内容を記入してください"
   puts"完了したらCtrl+Dを押します"
    
-  CSV.open("#{@file_name}.csv",'a') do |input|
-  @user_inputs = readlines
-  input << @user_inputs
+  CSV.open("#{file_name}.csv",'a') do |input|
+   user_inputs = readlines
+   input << user_inputs
   end   
  end
 
