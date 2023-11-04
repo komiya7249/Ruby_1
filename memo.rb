30f016d74a1c1443f6aef550f93807cc149eb914
komiya7249 <119317460+komiya7249@users.noreply.github.com>
2023-11-04 14:31:00 +0900

    異常値弾きをelseへ修正／カンマ、ダブルクォーテーションが入らないように修正／ファイル名に空欄（[?）が入らないように修正。

--- memo.rb
+++ memo.rb
@@ -5,31 +5,34 @@
  memo_type = gets.to_i 
  
  
- if memo_type != 1 && memo_type != 2 
-  puts "規定以外の値が入力されました。最初からやり直してください"
-  
- elsif memo_type == 1
+ if memo_type == 1
   puts "メモを新規作成します。拡張子を除いたファイル名を入力してください"
-  file_name = gets.to_s
+  file_name = gets.to_s.chomp
   puts"メモしたい内容を記入してください"
   puts"完了したらCtrl+Dを押します"
   
-  CSV.open("#{file_name}.csv",'a') do |input|
+  CSV.open("#{file_name}.csv",'w',col_sep:'',row_sep:'',quote_char:'') do |input|
    user_inputs = readlines
+   user_inputs.push "\n"   
    input << user_inputs
   end
   
-  puts "作成が完了しました"   
+  puts "\n作成が完了しました"   
    
  elsif memo_type == 2
   puts"既存のメモに書き込みを行います。拡張子を除いたファイル名を入力してください"
-  file_name = gets.to_s
+  file_name = gets.to_s.chomp
   puts"メモしたい内容を記入してください"
   puts"完了したらCtrl+Dを押します"
    
-  CSV.open("#{file_name}.csv",'a') do |input|
+  CSV.open("#{file_name}.csv",'a',col_sep:'',row_sep:'',quote_char:'') do |input|
    user_inputs = readlines
+   user_inputs.push "\n"
    input << user_inputs
-  end   
+  end
+ 
+  puts "\n編集が完了しました"     
+  
+ else
+   puts "規定以外の値が入力されました。最初からやり直してください"
  end
-
